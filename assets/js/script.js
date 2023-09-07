df8fc0d3c8714eecef848a6641089a89e68e9efb
widura26 <widurategalrejo@gmail.com>
2023-09-07 11:31:56 +0700

    fourth  commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const currentDate = new Date();
+
+const year = currentDate.getFullYear();
+const month = currentDate.getMonth() + 1;
+const day = currentDate.getDate();
+const hours = currentDate.getHours();
+const minutes = currentDate.getMinutes();
+const seconds = currentDate.getSeconds();
+
+const dateTime = document.querySelector('.date p');
+dateTime.innerHTML = `${hours}:${minutes} WIB`;
