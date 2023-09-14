568862e5c6db66aebfd73d19554faf3f356bcf14
widura26 <widurategalrejo@gmail.com>
2023-09-14 07:36:08 +0700

    add realtime clock

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,16 @@
-const currentDate = new Date();
+function clock(){
+    const currentDate = new Date();
+    
+    const year = currentDate.getFullYear();
+    const month = currentDate.getMonth() + 1;
+    const day = currentDate.getDate();
+    const hours = currentDate.getHours();
+    const minutes = currentDate.getMinutes();
+    const seconds = currentDate.getSeconds();
+    
+    const dateTime = document.querySelector('.date p');
+    dateTime.innerHTML = `${hours} : ${minutes} : ${seconds} WIB`;
+}
 
-const year = currentDate.getFullYear();
-const month = currentDate.getMonth() + 1;
-const day = currentDate.getDate();
-const hours = currentDate.getHours();
-const minutes = currentDate.getMinutes();
-const seconds = currentDate.getSeconds();
-
-const dateTime = document.querySelector('.date p');
-dateTime.innerHTML = `${hours}:${minutes} WIB`;
+clock();
+setInterval(clock, 1000);
